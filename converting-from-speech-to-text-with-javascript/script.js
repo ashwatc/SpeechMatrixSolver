91d498689ba0f4932bf00bc4f3f154c0decc5c55
Ashwat Chidambaram <ashwatc@berkeley.edu>
2019-11-09 01:04:47 -0800

    added matrix bounds detection, stored in array called 'bounds' for later

--- converting-from-speech-to-text-with-javascript/script.js
+++ converting-from-speech-to-text-with-javascript/script.js
@@ -99,7 +99,9 @@ $('#save-note-btn').on('click', function(e) {
   else {
     // Save note to localStorage.
     // The key is the dateTime with seconds, the value is the content of the note.
+
     saveNote(new Date().toLocaleString(), noteContent);
+    gauss(noteContent)
 
     // Reset variables and update UI.
     noteContent = '';
@@ -200,3 +202,55 @@ function deleteNote(dateTime) {
   localStorage.removeItem('note-' + dateTime); 
 }
 
+function gauss(inpStr) {
+  bounds = inpStr.split('by')
+  bounds[0] = bounds[0].substring(bounds[0].length - 2, bounds[0].length - 1)
+  bounds[0] = parseInt(bounds[0])
+  bounds[1] = bounds[1].substring(1, 2)
+  bounds[1] = parseInt(bounds[1])
+  console.log(bounds)
+
+  var n = A.length;
+
+  for (var i=0; i<n; i++) {
+      // Search for maximum in this column
+      var maxEl = Math.abs(A[i][i]);
+      var maxRow = i;
+      for(var k=i+1; k<n; k++) {
+          if (Math.abs(A[k][i]) > maxEl) {
+              maxEl = Math.abs(A[k][i]);
+              maxRow = k;
+          }
+      }
+
+      // Swap maximum row with current row (column by column)
+      for (var k=i; k<n+1; k++) {
+          var tmp = A[maxRow][k];
+          A[maxRow][k] = A[i][k];
+          A[i][k] = tmp;
+      }
+
+      // Make all rows below this one 0 in current column
+      for (k=i+1; k<n; k++) {
+          var c = -A[k][i]/A[i][i];
+          for(var j=i; j<n+1; j++) {
+              if (i==j) {
+                  A[k][j] = 0;
+              } else {
+                  A[k][j] += c * A[i][j];
+              }
+          }
+      }
+  }
+
+  // Solve equation Ax=b for an upper triangular matrix A
+  var x= new Array(n);
+  for (var i=n-1; i>-1; i--) {
+      x[i] = A[i][n]/A[i][i];
+      for (var k=i-1; k>-1; k--) {
+          A[k][n] -= A[k][i] * x[i];
+      }
+  }
+  // console.log(x)
+  return x;
+}
