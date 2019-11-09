36d53c89572ff398dd7536b1c961f45e6299e16a
Ashwat Chidambaram <ashwatc@berkeley.edu>
2019-11-09 14:35:33 -0800

    edited methods of matrix solver, creating seperate file to be edited by other members

--- MatrixSolver.js
+++ MatrixSolver.js
@@ -1,7 +1,6 @@
-// matrix solver https://martin-thoma.com/solving-linear-equations-with-gaussian-elimination/#python-code
-function gauss(A) {
+function gauss(rows, columns, A){
     var n = A.length;
-
+  
     for (var i=0; i<n; i++) {
         // Search for maximum in this column
         var maxEl = Math.abs(A[i][i]);
@@ -12,14 +11,14 @@ function gauss(A) {
                 maxRow = k;
             }
         }
-
+  
         // Swap maximum row with current row (column by column)
         for (var k=i; k<n+1; k++) {
             var tmp = A[maxRow][k];
             A[maxRow][k] = A[i][k];
             A[i][k] = tmp;
         }
-
+  
         // Make all rows below this one 0 in current column
         for (k=i+1; k<n; k++) {
             var c = -A[k][i]/A[i][i];
@@ -32,7 +31,7 @@ function gauss(A) {
             }
         }
     }
-
+  
     // Solve equation Ax=b for an upper triangular matrix A
     var x= new Array(n);
     for (var i=n-1; i>-1; i--) {
@@ -41,5 +40,12 @@ function gauss(A) {
             A[k][n] -= A[k][i] * x[i];
         }
     }
+    console.log(x)
+  
+    for (var i = 0; i < x.length; i++){
+      if (Number.isNaN(x[i])){
+        x[i] = "No solution"
+      }
+    }
     return x;
-}
+  }
