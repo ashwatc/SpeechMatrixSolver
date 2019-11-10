cd3d66b006522448236f7d4dac42fc1fc0b95931
Ashwat Chidambaram <ashwatc@berkeley.edu>
2019-11-09 18:06:29 -0800

    combined gaussian solver with front-end

--- SMS/script.js
+++ SMS/script.js
@@ -232,7 +232,7 @@ function stringToMatrices(inpStr){
         if (Number.isSafeInteger(val)){
           // val = parseInt(val)
           matrixRow.push(val)
-          console.log(matrixRow)
+          // console.log(matrixRow)
           column++
           if (column == bounds[1]){
             // inputMatrix[row] = matrixRow
@@ -250,59 +250,82 @@ function stringToMatrices(inpStr){
   // A = [[1, 0, 7], [0, 1, 4]]
   // *** A = [[1, 1, 0, 1, 21], [1, 1, 1, 0, 21], [0, 2, 3, 0, 37], [2, 1, 0, 0, 19]] ***
 
-  console.log(inputMatrix)
+  // console.log(inputMatrix)
   gauss(bounds[0], bounds[1], inputMatrix)
 }
 
-function gauss(rows, columns, A){
-  var n = A.length;
+// A = [[1, 2, 3], [2, -1, 1], [3, 1, 4]]
+// console.log(gauss(3, 3, A))
 
-  for (var i=0; i<n; i++) {
-      // Search for maximum in this column
-      var maxEl = Math.abs(A[i][i]);
-      var maxRow = i;
-      for(var k=i+1; k<n; k++) {
-          if (Math.abs(A[k][i]) > maxEl) {
-              maxEl = Math.abs(A[k][i]);
-              maxRow = k;
-          }
-      }
-
-      // Swap maximum row with current row (column by column)
-      for (var k=i; k<n+1; k++) {
-          var tmp = A[maxRow][k];
-          A[maxRow][k] = A[i][k];
-          A[i][k] = tmp;
-      }
-
-      // Make all rows below this one 0 in current column
-      for (k=i+1; k<n; k++) {
-          var c = -A[k][i]/A[i][i];
-          for(var j=i; j<n+1; j++) {
-              if (i==j) {
-                  A[k][j] = 0;
-              } else {
-                  A[k][j] += c * A[i][j];
-              }
-          }
-      }
-  }
-
-  // Solve equation Ax=b for an upper triangular matrix A
-  var x= new Array(n);
-  for (var i=n-1; i>-1; i--) {
-      x[i] = A[i][n]/A[i][i];
-      for (var k=i-1; k>-1; k--) {
-          A[k][n] -= A[k][i] * x[i];
-      }
-  }
-  console.log("the answer is...")
-  console.log(x)
-
-  for (var i = 0; i < x.length; i++){
-    if (Number.isNaN(x[i])){
-      x[i] = "No solution"
+function gauss(rows, columns, A) {
+    var n = A.length;
+    
+    if (n !== rows) {  // Number of expected rows != number of rows
+        var x = "The dimensions of the expected matrix are different from the dimensions of the given matrix!"
+        return x
     }
-  }
-  return x;
+
+    for (var i = 0; i < n - 1; i ++) {
+        if (A[i].length !== A[i + 1].length) {
+            var x = "The dimensions of the expected matrix are different from the dimensions of the given matrix!"
+            return x
+        }
+
+    }
+
+    for (var i=0; i<n; i++) {
+        // Search for maximum in this column
+        var maxEl = Math.abs(A[i][i]);
+        var maxRow = i;
+        for(var k=i+1; k<n; k++) {
+            if (Math.abs(A[k][i]) > maxEl) {
+                maxEl = Math.abs(A[k][i]);
+                maxRow = k;
+            }
+        }
+
+        // Swap maximum row with current row (column by column)
+        for (var k=i; k<n+1; k++) {
+            var tmp = A[maxRow][k];
+            A[maxRow][k] = A[i][k];
+            A[i][k] = tmp;
+        }
+
+        // Make all rows below this one 0 in current column
+        for (k=i+1; k<n; k++) {
+            var c = -A[k][i]/A[i][i];
+            for(var j=i; j<n+1; j++) {
+                if (i==j) {
+                    A[k][j] = 0;
+                } else {
+                    A[k][j] += c * A[i][j];
+                }
+            }
+        }
+    }
+
+    var zeroes = []
+    for (var i = 0; i < A[0].length - 1; i ++) {    //checking simplified matrix for dependency
+        zeroes.push(0)
+    }
+    for (var i = 0; i < n; i ++) {
+        if (JSON.stringify(A[i].slice(0, A[0].length - 1)) == JSON.stringify(zeroes)){ //if LHS is zeroes only (ie. row of zeroes for LHS) 
+            if (A[i][A[0].length - 1] == 0){ //if 0 = 0
+                //if (A.slice())
+                return gauss(rows-1, columns, A.splice(i,1))
+            }else { //if 0 = 1 or something
+                return "No solutions" // check for row comparison
+                }
+        }
+    }
+    // Solve equation Ax=b for an upper triangular matrix A
+    var x= new Array(n);
+    for (var i=n-1; i>-1; i--) {
+        x[i] = A[i][n]/A[i][i];
+        for (var k=i-1; k>-1; k--) {
+            A[k][n] -= A[k][i] * x[i];
+        }
+    }
+    console.log(x)
+    return x;
 }
